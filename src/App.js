36200009473efd0c01eac77651d57852b5139f46
vishal marhatta <vishal.marhatta@gmail.com>
2018-09-15 18:40:58 +0530

    Added vertical scroll to cards and fetched robots from json placeholder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React ,{Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import {robots } from './robots';
+import Scroll from './Scroll';
 import './App.css';
 
 class App extends Component{
@@ -10,9 +10,16 @@ class App extends Component{
 	{
 		super()
 		this.state={
-			robots:robots,
+			robots:[],
 			searchField:""
 		}
+	}
+
+	componentDidMount()
+	{
+		fetch("https://jsonplaceholder.typicode.com/users")
+		.then(response=>response.json())
+		.then(users=> this.setState({robots:users}));
 	}
 
 	onSearchChange=(event)=>{
@@ -26,13 +33,24 @@ class App extends Component{
 			return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
 		});
 
-		return(
-		<div className="tc">
-			<h1 className="f1">robofriends</h1>
-			<SearchBox searchChange={this.onSearchChange}/>
-			<CardList robots={filteredRobots}/>
-		</div>
-		);
+		if(this.state.robots.length===0)
+		{
+			return <h1 className="tc">Loading</h1>
+		}
+		else
+		{
+
+			return(
+				<div className="tc">
+					<h1 className="f1">robofriends</h1>
+					<SearchBox searchChange={this.onSearchChange}/>
+					<Scroll>
+						<CardList robots={filteredRobots}/>
+					</Scroll>
+				</div>
+				);
+		}
+
 	}
 	
 
