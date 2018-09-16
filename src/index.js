93d83df0710ff7983aedbe4daf2766583e9558d6
vishal marhatta <vishal.marhatta@gmail.com>
2018-09-17 01:03:56 +0530

    connected react with redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom'; //virtual dom created by react
+import { Provider ,connect } from "react-redux";
+import { createStore } from "redux";
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import 'tachyons';
 import registerServiceWorker from './registerServiceWorker';
+import { searchRobots } from "./reducers";
 
 
-ReactDOM.render(<App />,document.getElementById('root'));
+const store = createStore(searchRobots)
+
+ReactDOM.render(<Provider store={store}><App /></Provider>,document.getElementById('root'));
 registerServiceWorker();
